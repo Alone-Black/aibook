ac0c73a77ff2ce0e4019dfbd9eaae4d49aaa5caa
Omid <omid87.87valizadeh@gmail.com>
2026-09-17 15:26:37 +0330

    omid add template for log

---
+++ login.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def login():
+    username = username_entry.get()
+    password = password_entry.get()
+    if username == "admin" and password == "password":
+        messagebox.showinfo("login","login successful")
+    else:
+        messagebox.showerror("login","invalid username or password")
+
+root=tk.TK()
+root.title("login")

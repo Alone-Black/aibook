c83b2f2a4ccdf62d4ee3cd1311973e35b7cfc0b8
Omid <omid87.87valizadeh@gmail.com>
2026-09-17 15:28:02 +0330

    omid edit loging bash

--- login.py
+++ login.py
@@ -1,13 +1,14 @@
-import tkinter as tk
-from tkinter import messagebox
+# import tkinter as tk
+# from tkinter import messagebox
 
-def login():
-    username = username_entry.get()
-    password = password_entry.get()
-    if username == "admin" and password == "password":
-        messagebox.showinfo("login","login successful")
-    else:
-        messagebox.showerror("login","invalid username or password")
+# def login():
+#     username = username_entry.get()
+#     password = password_entry.get()
+#     if username == "admin" and password == "password":
+#         messagebox.showinfo("login","login successful")
+#     else:
+#         messagebox.showerror("login","invalid username or password")
 
-root=tk.TK()
-root.title("login")
+# root=tk.TK()
+# root.title("login")
+print("hello")
